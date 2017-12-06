99a7a6ae08f0efb3239649cee280b65d5f489238
Nathan Brewer-Davis <10ndavis10@gmail.com>
2017-12-05 21:16:44 -0600

    updated drawer
    updated drawer

--- Components/Drawer.js
+++ Components/Drawer.js
@@ -16,17 +16,16 @@ export default class Drawer extends React.Component {
     return (
       <View style={styles.drawer}>
         { screenProps.loginStatus === true
-          ? <View>
-              <View style={styles.group}>
-                <Text style={styles.listItem} onPress={this.navigateToScreen('Binders')}>Binders</Text>
-                <Text style={styles.listItem} onPress={this.navigateToScreen('Trade')}>Check Trade List</Text>
-                <Text style={styles.listItem} onPress={this.navigateToScreen('WishList')}>Share Wish List</Text>
-              </View>
-              <View style={styles.logout}><Text style={styles.listItem} onPress={this.navigateToScreen('LoginLogout')}>Logout</Text></View>
+          ?
+            <View style={styles.group}>
+              <View style={styles.drawerHeader}></View>
+              <View style={styles.listItem}><Text onPress={this.navigateToScreen('Binders')}>Binders</Text></View>
+              <View style={styles.listItem}><Text onPress={this.navigateToScreen('Trade')}>Check Trade List</Text></View>
+              <View style={styles.listItem}><Text onPress={this.navigateToScreen('LoginLogout')}>Logout</Text></View>
             </View>
           :
             <View>
-              <Text style={styles.listItem} onPress={this.navigateToScreen('LoginLogout')}>Login</Text>
+              <View style={styles.listItem}><Text onPress={this.navigateToScreen('LoginLogout')}>Login</Text></View>
             </View>
         }
       </View>
@@ -38,17 +37,26 @@ const styles = StyleSheet.create({
   drawer: {
     flex: 1,
     flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  drawerHeader: {
+    height: 150,
+    alignSelf: 'stretch',
+    backgroundColor: 'gray'
   },
   group: {
     flex: 1,
     width: 300
   },
   listItem: {
-    margin: 10
+    justifyContent: 'center',
+    margin: 10,
+    backgroundColor: '#E1E2E1',
+    alignSelf: 'stretch',
+    height: 50
   },
   logout: {
     flex: 1,
-    flexDirection: 'row',
     alignItems: 'flex-end',
     width: 300
   }
